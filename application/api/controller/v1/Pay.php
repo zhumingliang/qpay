166dc0e82dc38763b9e9df8a2b102653ae497e33
[zhumingliang] <[mingliangzhu@aliyun.com]>
2018-12-21 13:21:14 +0800

    项目初始化

--- application/api/controller/v1/Pay.php
+++ application/api/controller/v1/Pay.php
@@ -32,90 +32,10 @@ class Pay extends BaseController
      * @throws \app\lib\exception\QpayException
      */
     public function payQCode($type = 800201, $id)
-   // public function payQCode()
     {
+        $res = (new PayService())->qCode($type, $id);
+        return json($res);
 
- /*       $url = 'https://openapi.qfpay.com';
-        $api_type = '/trade/v1/payment';
-        $mchid = "eqqmYMn0Zj6pncw5ZDxjgMqbzV"; //錢方提供的mchid
-        $app_code = 'D75D13EE4719452698FA08359C28F63A'; //錢方提供的App Code
-        $app_key = 'B6BF325B9E514FFDA1C6474C61BC9F87'; //錢方提供的App Key
-        $now_time = date("Y-m-d H:i:s"); //獲取當前時間
-
-        $fields_string = '';
-        $fields = array(
-            // 'mchid' => urlencode($mchid),
-            'out_trade_no' => $this->GetRandStr(20),
-            'pay_type' => 801501,
-            'txamt' => 10,
-            'txcurrcd' => 'HKD',
-            'goods_name' => '测试商品',
-            'txdtm' => $now_time
-        );
-
-        ksort($fields); //字典排序A-Z升序方式
-
-        foreach ($fields as $key => $value) {
-            $fields_string .= $key . '=' . $value . '&';
-        }
-        $fields_string = substr($fields_string, 0, strlen($fields_string) - 1); //刪除最後一個 & 符號
-
-        $sign = strtoupper(md5($fields_string . $app_key));
-
-        //// 設置Header ////
-        $header = array();
-        $header[] = 'X-QF-APPCODE: ' . $app_code;
-        $header[] = 'X-QF-SIGN: ' . $sign;
-
-        //Post Data去錢方海外URL
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url . $api_type);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-        $output = curl_exec($ch);
-        curl_close($ch);
-
-        //打印結果信息
-        header('Content-type:text/json');
-        $final_data = json_decode($output, true);
-       return $this->qrcode($final_data['pay_url']);*/
-
-          $res = (new PayService())->qCode($type,$id);
-          return json($res);
-
-    }
-
-
-    // 二维码
-    public function qrcode($qrData)
-    {
-        $savePath = dirname($_SERVER['SCRIPT_FILENAME']) . '/static/qrcode/';
-
-        // $qrData = 'http://www.cnblogs.com/nickbai/';
-        $qrLevel = 'H';
-        $qrSize = '8';
-        $savePrefix = 'NickBai';
-        $filename = createQRcode($savePath, $qrData, $qrLevel, $qrSize, $savePrefix);
-
-        return config('setting.img_prefix') . 'static/qrcode/' . $filename;
-
-
-    }
-
-
-    public function GetRandStr($length)
-    {
-        $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-        $len = strlen($str) - 1;
-        $randstr = '';
-        for ($i = 0; $i < $length; $i++) {
-            $num = mt_rand(0, $len);
-            $randstr .= $str[$num];
-        }
-        return $randstr;
     }
 
     public function receiveNotify()
--- application/api/service/PayService.php
+++ application/api/service/PayService.php
@@ -27,14 +27,27 @@ class PayService
     {
         $info = (new OrderService())->getOrderInfo($id);
         $qpayParam = new QpayDataBase();
-        $qpayParam->setTxamt($info['txamt']*100);//订单支付金额，单位分；
-        $qpayParam->setTxcurrcd('CNY');//币种    港币：HKD ；人民币：CNY；日元：JPY；美元：USD；迪拉姆：AED；泰铢：THB
-        $qpayParam->setPayType(800201);// 微信扫码:800201；支付宝扫码:800101
-        $qpayParam->setOuTradeNo(urlencode(time()));// 外部订单号，开发者平台订单号，同子商户（mchid）下，每次成功调用支付（含退款）接口，该参数值均不能重复使用,保证单号唯一，长度不超过128字符
-        $qpayParam->setTxdtm(date('Y-m-d H:i:s'));// 请求交易时间格式为：格式为：YYYY-MM-DD HH:MM:SS
-        $qpayParam->setGoodsName($info['goods_name']);//商品名称或标示，建议不超过20字，不含英文逗号等特殊字符
-        // $qpayParam->setPayTag(urlencode($info['pay_tag']));//商品名称或标示，建议不超过20字，不含英文逗号等特殊字符
-        $res = QPayUnifiedOrder::unifiedOrder($qpayParam);
+        if ($id){
+            $qpayParam->setTxamt(200);//订单支付金额，单位分；
+            $qpayParam->setTxcurrcd('HKD');//币种    港币：HKD ；人民币：CNY；日元：JPY；美元：USD；迪拉姆：AED；泰铢：THB
+            $qpayParam->setPayType(801501);// 微信扫码:800201；支付宝扫码:800101
+            $qpayParam->setOuTradeNo(urlencode(time()));// 外部订单号，开发者平台订单号，同子商户（mchid）下，每次成功调用支付（含退款）接口，该参数值均不能重复使用,保证单号唯一，长度不超过128字符
+            $qpayParam->setTxdtm(date('Y-m-d H:i:s'));// 请求交易时间格式为：格式为：YYYY-MM-DD HH:MM:SS
+            $qpayParam->setGoodsName($info['goods_name']);//商品名称或标示，建议不超过20字，不含英文逗号等特殊字符
+            $qpayParam->setPayTag('ALIPAYCN');//商品名称或标示，建议不超过20字，不含英文逗号等特殊字符
+
+        }else{
+            $qpayParam->setTxamt($info['txamt']*100);//订单支付金额，单位分；
+            $qpayParam->setTxcurrcd('CNY');//币种    港币：HKD ；人民币：CNY；日元：JPY；美元：USD；迪拉姆：AED；泰铢：THB
+            $qpayParam->setPayType(800201);// 微信扫码:800201；支付宝扫码:800101
+            $qpayParam->setOuTradeNo(time());// 外部订单号，开发者平台订单号，同子商户（mchid）下，每次成功调用支付（含退款）接口，该参数值均不能重复使用,保证单号唯一，长度不超过128字符
+            $qpayParam->setTxdtm(date('Y-m-d H:i:s'));// 请求交易时间格式为：格式为：YYYY-MM-DD HH:MM:SS
+            $qpayParam->setGoodsName($info['goods_name']);//商品名称或标示，建议不超过20字，不含英文逗号等特殊字符
+            //$qpayParam->setPayTag('ALIPAYCN');//商品名称或标示，建议不超过20字，不含英文逗号等特殊字符
+            $res = QPayUnifiedOrder::unifiedOrder($qpayParam);
+        }
+
+       // print_r($res);
 
         $data = $qpayParam->getValues();
         $data['q_res'] = json_encode($res);
@@ -42,7 +55,6 @@ class PayService
         if ($res->respcd != '0000') {
             throw  new QpayException();
         }
-
 
         if ($res->pay_type == '801501') {
             $qrcode = $res->pay_url;
